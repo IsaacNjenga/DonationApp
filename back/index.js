e3394f5b776b7e2e4e15176e20df25216c3284b8
Isaac Njenga <ayzzoh20@gmail.com>
2024-11-27 11:59:58 +0300

    minor change

--- back/index.js
+++ back/index.js
@@ -5,7 +5,7 @@ import { Router } from "./routes/routes.js";
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000", "https://donation-app-front.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
--- front/src/App.js
+++ front/src/App.js
@@ -4,7 +4,8 @@ import axios from "axios";
 import Home from "../src/pages/home";
 import Donate from "../src/pages/donate";
 
-axios.defaults.baseURL = "http://localhost:3001/donate";
+//axios.defaults.baseURL = "http://localhost:3001/donate";
+axios.defaults.baseURL = "https://donation-app-umber.vercel.app/donate";
 axios.defaults.withCredentials = true;
 
 const router = createBrowserRouter([
